41abeaea54c1605304536ab5191556f9e9013d62
KINGbabasula <kingbabasula@gmail.com>
2014-04-28 14:13:50 +0200

    Clean up

--- patch.sh
+++ patch.sh
@@ -1,34 +1,11 @@
 #!/bin/bash
 
-#rm -r frameworks/av
-#rm -r frameworks/native
 rm -r bootable/recovery
 rm -r hardware/libhardware_legacy
 rm -r packages/inputmethods/LatinIME
 rm -r packages/services/Telephony
-#rm -r system/core
-#rm -r system/vold
 
 repo sync -l
-
-#echo "android_frameworks_av"
-#cp patches/frameworks_av.patch frameworks/av/frameworks_av.patch
-#cd frameworks/av
-#git apply frameworks_av.patch
-#rm frameworks_av.patch
-#git revert ff7ae18ca39ced274f37856b8da9dc1e446f69dd
-
-#cd ../..
-
-#echo ""
-
-#echo "android_frameworks_native"
-#cp patches/frameworks_native.patch frameworks/native/frameworks_native.patch
-#cd frameworks/native
-#git apply frameworks_native.patch
-#rm frameworks_native.patch
-#git revert 8124b181d4b5a3a44796fdb0e3ea4e4171f102c7
-#cd ../..
 
 echo ""
 
@@ -70,21 +47,3 @@ rm Telephony.patch
 cd ../../..
 
 echo ""
-
-#echo "system/core fix"
-#cp patches/system_core.patch system/core/system_core.patch
-#cd system/core
-#git apply system_core.patch
-#rm system_core.patch
-#cd ../..
-
-#echo ""
-
-#echo "system/vold fix"
-#cp patches/system_vold.patch system/vold/system_vold.patch
-#cd system/vold
-#git apply system_vold.patch
-#rm system_vold.patch
-#cd ../..
-
-#echo ""
