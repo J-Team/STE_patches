ec16b34a9f5f104b0ba50c35d23a341e9fe89b74
Alberto97 <albertop2197@gmail.com>
2014-06-09 12:26:16 +0200

    Send all files by Bluetooth
    Change-Id: If178f146b36ad88c3bdcc4c0f515a42c3966e2af

--- patch.sh
+++ patch.sh
@@ -4,6 +4,7 @@ rm -r bootable/recovery
 rm -r hardware/libhardware_legacy
 rm -r packages/inputmethods/LatinIME
 rm -r packages/services/Telephony
+rm -r packages/apps/Bluetooth
 
 repo sync -l
 
@@ -44,6 +45,15 @@ cp patches/Telephony.patch packages/services/Telephony/Telephony.patch
 cd packages/services/Telephony
 git apply Telephony.patch
 rm Telephony.patch
+cd ../../..
+
+echo ""
+
+echo "Send all files by Bluetooth"
+cp patches/BT.patch packages/apps/Bluetooth/BT.patch
+cd packages/apps/Bluetooth
+git apply BT.patch
+rm BT.patch
 cd ../../..
 
 echo ""
